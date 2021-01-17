a081b9c959a1db99d3f85c9f71701b89f5c368a0
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2021-01-17 03:33:02 +0000

    Better function structure for the module

--- ot_query.py
+++ ot_query.py
@@ -3,7 +3,7 @@
 import argparse
 import sys
 
-import opentargets
+import opentargets as ot
 import pandas as pd
 import retry
 
@@ -14,82 +14,122 @@ if sys.version_info < (3, 6):
 
 # Configure Pandas settings for complete dataframe printouts
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+pd.set_option('display.max_rows', 10)
 pd.set_option('max_colwidth', None)
 pd.set_option('display.expand_frame_repr', False)
 
 # Set up the argument parser but don't parse just yet
 parser = argparse.ArgumentParser('Query the Open Targets REST API by target (gene) and/or by disease and calculate '
-                                 'simple statistics for the association_score.overall: min, max, avg and stdev. Both '
-                                 'parameters are optional, but at least one must be specified. When both parameters are'
-                                 'specified, the target and disease analyses are run separately.')
+                                 'simple statistics for the association_score.overall: min, max, avg and stdev. When '
+                                 'both parameters are specified, the target and disease analyses are run separately.')
 parser.add_argument('-t', '--target', required=False, help='Target (gene) ID, e.g. ENSG00000197386.')
 parser.add_argument('-d', '--disease', required=False, help='Disease ID, e.g. Orphanet_399.')
 
 
 @retry.retry(tries=5, delay=5, backoff=1.2, jitter=(1, 3))
-def query_rest_api(ot_client, filter_type: str, filter_value: str) -> pd.DataFrame:
+def query_rest_api(ot_client: ot.OpenTargetsClient, filter_type: str, filter_value: str) -> list:
     """Query the Open Targets REST API with the appropriate filters. Retry if necessary in case of network errors.
 
     Args:
         ot_client: Instance of an Open Targets client for querying.
-        filter_type: which field to filter by. Can be either 'target' or 'disease'.
-        filter_value: which value to filter by in a given filter type.
+        filter_type: Filter name to query by, can be 'target' or 'disease'.
+        filter_value: The value for the selected filter.
 
-    Return:
-        A Pandas dataframe with target_id, disease_id, and association_score.overall columns, one row per one entry
-        in the query result. Possibly empty if nothing is found.
+    Returns:
+        * If the filter value is unspecified, None.
+        * Otherwise, a list of association results (can be empty if nothing was found).
     """
-    associations = list(ot_client.filter_associations(**{filter_type: filter_value}))
+    return list(ot_client.filter_associations(**{filter_type: filter_value})) if filter_value else None
 
-    # Construct the dataframe. Since Pandas doesn't yet support a composite dtype in a dataframe constructor, the
-    # easiest way (which would not rely on dtype autodetection) is to supply each column as a separate series with
-    # explicit dtype declaration.
-    association_df = pd.DataFrame({
-        'target_id': pd.Series([a['target']['id'] for a in associations], dtype='str'),
-        'disease_id': pd.Series([a['disease']['id'] for a in associations], dtype='str'),
-        'association_score_overall': pd.Series([a['association_score']['overall'] for a in associations], dtype='float')
-    })
 
-    return association_df
+def get_associations(ot_client: ot.OpenTargetsClient, target_id: str, disease_id: str) -> dict:
+    """Based on target and/or disease ID, perform the necessary queries and format the association data in the form
+    of a Pandas dataframe.
+
+    Args:
+        ot_client: Instance of an Open Targets client for querying.
+        target_id: Target (gene) ID as understood by the Open Targets API, e.g. ENSG00000197386.
+        disease_id: Disease ID as understood by the Open Targets API, e.g. Orphanet_399.
+
+    Returns:
+        A dict with two keys ('target' and 'disease'). The value for each field will be:
+        * If the corresponding filter value is unspecified, None
+        * Otherwise, a Pandas dataframe with three columns: target_id, disease_id, score_overall. The dataframe can be
+          empty if no results were found.
+    """
+    association_result = dict()
+    for filter_type, filter_value in (('target', target_id), ('disease', disease_id)):
+        associations = query_rest_api(ot_client, filter_type, filter_value)
+        if associations is None:
+            result = None
+        else:
+            result = pd.DataFrame({
+                'target_id': pd.Series([a['target']['id'] for a in associations], dtype='str'),
+                'disease_id': pd.Series([a['disease']['id'] for a in associations], dtype='str'),
+                'score_overall': pd.Series([a['association_score']['overall'] for a in associations], dtype='float')
+            })
+            # Verify API sanity
+            assert set(result[filter_type + '_id']) == {filter_value}, 'API returned inconsistent results'
+        association_result[filter_type] = result
+    return association_result
+
+
+def print_association_data(associations: dict) -> None:
+    """Prints the raw association data, one row per entry returned by query.
+
+    Args:
+        associations: The association dict, as returned by get_associations().
+    """
+    for filter_type, results in associations.items():
+        print(f'Query results for {filter_type}:')
+        if results is None:
+            print('No filter was specified for this query.')
+        elif len(results) == 0:
+            print('The query did not return any results.')
+        else:
+            print(results)
+        print()
+
+
+def print_summary_metrics(associations: dict) -> None:
+    """Prints the summary metrics for the association data.
+
+    Args:
+        associations: The association dict, as returned by get_associations().
+    """
+    for filter_type, results in associations.items():
+        print(f'Summary results for {filter_type}:')
+        if results is None:
+            print('No filter was specified for this query.')
+        elif len(results) == 0:
+            print('The query did not return any results.')
+        else:
+            print(f'Minimum = {results.score_overall.min():.3f}')
+            print(f'Maximum = {results.score_overall.max():.3f}')
+            print(f'Mean = {results.score_overall.mean():.3f}')
+            print(f'Standard deviation = {results.score_overall.std():.3f}')
+            print()
 
 
 def main(target_id: str, disease_id: str) -> None:
-    """Do the queries, print the full results first, and then the summary statistics. Each parameter is optional but
-    at least one must be specified.
+    """Do the queries, print the full results first, and then the summary statistics.
 
     Args:
         target_id: Target (gene) ID as understood by the Open Targets API, e.g. ENSG00000197386.
         disease_id: Disease ID as understood by the Open Targets API, e.g. Orphanet_399.
         """
 
-    # Check if we have anything to query
-    if not (target_id or disease_id):
-        sys.exit('At least one of (target ID, disease ID) must be specified')
+    # Init the Open Targets client
+    ot_client = ot.OpenTargetsClient()
 
-    # Init the Open Targets client and do the query. Store the dataframes rather than printing immediately, because we
-    # need to first print the raw results, and then separately summary statistics per query type.
-    ot_client = opentargets.OpenTargetsClient()
-    associations = {
-        filter_type: query_rest_api(ot_client, filter_type, filter_value)
-        for filter_type, filter_value in (('target', target_id), ('disease', disease_id))
-        if filter_value
-    }
+    # Get the associations
+    associations = get_associations(ot_client, target_id, disease_id)
 
     # Print the associations
-    for filter_type, results in associations.items():
-        print(f'Query results for {filter_type}:')
-        print(results)
-        print()
+    print_association_data(associations)
 
     # Print the summary statistics
-    for filter_type, results in associations.items():
-        print(f'Summary results for {filter_type}:')
-        print(f'Minimum = {results.association_score_overall.min():.3f}')
-        print(f'Maximum = {results.association_score_overall.max():.3f}')
-        print(f'Mean = {results.association_score_overall.mean():.3f}')
-        print(f'Standard deviation = {results.association_score_overall.std():.3f}')
-        print()
+    print_summary_metrics(associations)
 
 
 if __name__ == '__main__':
